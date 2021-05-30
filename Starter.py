f74021c286eba92cd9dc620385807f34b8ac05f8
1-Mustafa <80366216+1-Mustafa@users.noreply.github.com>
2021-05-31 10:20:57 +1200

    Add files via upload

---
+++ Starter.py
@@ -0,0 +1,11 @@
+#Ask the user for their name and stor it in a variable
+name = input("What  is your name")
+#Print the user's name
+print(name)
+#Ask for the user's for their age
+age = int(input("How old are you"))
+#print the user's age
+print(age)
+#Ask the user what's their antionality
+nationality = input("What is your nationality")
+#print the user's nationality
