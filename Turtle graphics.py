25fb21911bad292f26799bd476793c25ffc59737
1-Mustafa <80366216+1-Mustafa@users.noreply.github.com>
2021-06-01 11:50:55 +1200

    Add files via upload

---
+++ Turtle graphics.py
@@ -0,0 +1,19 @@
+import turtle
+Screen = turtle.Turtle()
+cir= ['red','red','red','red','red']
+turtle.pensize(4)
+turtle.penup()
+turtle.setpos(-90,30)
+turtle.pendown()
+for i in range(5):
+	turtle.pencolor(cir[i])
+	turtle.forward(200)
+	turtle.right(144)
+
+turtle.penup()
+turtle.setpos(80,-140)
+turtle.pendown()
+
+turtle.pencolor("Black")
+turtle.done()
+
